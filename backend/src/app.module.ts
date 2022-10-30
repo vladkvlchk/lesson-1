5122e02bc9a0045f44c1c5e0ed5f28565706f7b0
Vladyslav <prostoandrei9@gmail.com>
2022-10-30 18:05:54 +0200

    fix-commit

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
@@ -20,6 +21,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
         logging: true
       })
     }),
+    UsersModule,
   ],
   providers: [],
 })
