977689a3f6b70c8f58d2aaa379e654379c2baabe
Vladyslav <prostoandrei9@gmail.com>
2022-10-30 16:45:34 +0200

    add docker, connect postgress

---
+++ backend/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, envFilePath: '../.env' }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) =>  ({
+        type: config.get<any>('TYPEORM_CONNECTION'),
+        username: config.get<string>('TYPEORM_USERNAME'),
+        password: config.get<string>('TYPEORM_PASSWORD'),
+        database: config.get<string>('TYPEORM_DATABASE'),
+        port: config.get<number>('TYPEORM_PORT'),
+        entities: [__dirname + "dist/**/*.entities{.ts, .js}"],
+        synchronize: true,
+        autoLoadEntities: true,
+        logging: true
+      })
+    }),
+  ],
+  providers: [],
+})
+export class AppModule {}
