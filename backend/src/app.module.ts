f14e3264dbbac64f97e02a11697fba76d65ad779
Vladyslav <prostoandrei9@gmail.com>
2022-10-30 21:16:10 +0200

    stopped on 42:11 for switch project

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,25 +1,31 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { GraphQLModule } from '@nestjs/graphql';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({ isGlobal: true, envFilePath: '../.env' }),
+    GraphQLModule.forRoot({
+      autoSchemaFile: 'schema.gql',
+      sortSchema: true,
+      playground: true,
+    }),
     TypeOrmModule.forRootAsync({
       imports: [ConfigModule],
       inject: [ConfigService],
-      useFactory: async (config: ConfigService) =>  ({
+      useFactory: async (config: ConfigService) => ({
         type: config.get<any>('TYPEORM_CONNECTION'),
         username: config.get<string>('TYPEORM_USERNAME'),
         password: config.get<string>('TYPEORM_PASSWORD'),
         database: config.get<string>('TYPEORM_DATABASE'),
         port: config.get<number>('TYPEORM_PORT'),
-        entities: [__dirname + "dist/**/*.entities{.ts, .js}"],
+        entities: [__dirname + 'dist/**/*.entities{.ts, .js}'],
         synchronize: true,
         autoLoadEntities: true,
-        logging: true
-      })
+        logging: true,
+      }),
     }),
     UsersModule,
   ],
---
+++ backend/src/users/services/user/user.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { Repository } from 'typeorm';
+
+@Injectable()
+export class UserService {
+    constructor(
+        @InjectRepository(UserEntity)
+        private readonly userRepository: Repository<UserEntity>
+    ){}
+}
